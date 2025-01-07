a4d660f9b169fce31a072c48b6290b59e538fd76
Mominul <ulhouqemomin39@gmail.com>
2025-01-07 16:00:48 +0600

    Learning if else condition

--- main.go
+++ main.go
@@ -1,7 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	fmt.Println("hello world")
+	// fmt.Println("hello world")
+
+	// a := 12
+	// fmt.Println(a)
+
+	// If else condition use
+	/* var a = 18
+
+	if a > 18 {
+		fmt.Println("You are eligiable for married")
+	} else if a < 18 {
+		fmt.Println("You are not eligable not married")
+	} else {
+		fmt.Println("Now You are teenager OKH")
+	}
+
+	*/
+
+	var name = 243
+
+	if name >= 32 {
+		fmt.Println("This is biggest number")
+
+	} else if name <= 34 {
+		fmt.Println("This is lowest number", +name)
+	} else {
+		fmt.Println("Not this exucuted")
+	}
+	//
+
 }
